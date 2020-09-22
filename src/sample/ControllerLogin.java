4215f722cfe40179e6195aa0cd268c16c130f2ed
Bruna Torres <bcristorres@gmail.com>
2020-09-22 18:51:50 -0300

    login structure

---
+++ src/sample/ControllerLogin.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControllerLogin {
+
+    /*
+    public void rememberPassword(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ScreenPassword.fxml"));
+        Stage primaryStage = new Stage();
+
+        primaryStage.setTitle("ScreenPassword");
+        primaryStage.setScene(new Scene(root, 790.0, 517.0));
+        primaryStage.
+        final Node source = (Node) actionEvent.getSource();
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
+    */
+
+    public void login(ActionEvent actionEvent) {
+
+    }
+
+    public void newUser(ActionEvent actionEvent) {
+
+    }
+}
+
