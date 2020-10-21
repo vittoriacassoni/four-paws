ea7ecb87e1f0e6ca518923f8ded6726dc6c52a18
alveslaura <laura.alvesfer@gmail.com>
2020-10-21 19:48:23 -0300

    adding the button back on the screenAdoptionRe1queriment

--- src/sample/ControllerAdoptionRequirement.java
+++ src/sample/ControllerAdoptionRequirement.java
@@ -1,4 +1,19 @@
 package sample;
 
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class ControllerAdoptionRequirement {
+
+
 }
