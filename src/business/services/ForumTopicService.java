30f17bb1f5ed3abe193b69830355831b60a22786
Mariana Carnevale <marianacarnevale@MacBook-Air-de-Mariana.local>
2020-11-18 21:34:15 -0300

    Fixing and updaating Forum

--- src/business/services/ForumTopicService.java
+++ src/business/services/ForumTopicService.java
@@ -1,5 +1,17 @@
 package business.services;
 
+import comuns.access.ForumTopic;
+import dao.access.ForumTopicSqlServerDAO;
+
+import java.util.ArrayList;
+
 public class ForumTopicService {
+
+    ForumTopicSqlServerDAO dao = new ForumTopicSqlServerDAO();
+
     //TODO - CHAMADA DOS METODOS DA DAO E VALIDAÇÕES
+
+    public ArrayList<ForumTopic> listTopics() {
+        return dao.listTopics();
+    }
 }
